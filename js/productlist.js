cd759209a2f3062550568cc1cd168f26255500d0
unknown <joelaaronross>
2018-10-11 13:39:47 -0700

    new update

--- js/productlist.js
+++ js/productlist.js
@@ -1,4 +1,26 @@
 $().ready(function(){
 
+    var url="http://localhost:8080/Product/List";
 
+    $.getJSON(url)
+    .then(function(jsonResponse) {
+        console.log(jsonResponse);
+        render(jsonResponse);
+        
+    });
+
+    function render(jsonResponse) {
+        var tbody = $("#tbody");
+        var products = jsonResponse.data;
+        
+        for(var product of products) {
+            var row = "<tr>";
+            row += "<td>" + product.id + "</td>";
+            row += "<td>" + product.name + "</td>";
+            row += "<td>" + product.price + "</td>";
+            row += "<td>" + product.vendor.name + "</td>";
+            row += "</tr>";
+            tbody.append(row);
+        }
+    }
 });
